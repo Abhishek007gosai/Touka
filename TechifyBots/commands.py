fe807c72d37338248d121265b643641143b4ad50
Abhishek007gosai <99321716+Abhishek007gosai@users.noreply.github.com>
2026-07-11 02:11:27 +0530

    Update commands.py

--- TechifyBots/commands.py
+++ TechifyBots/commands.py
@@ -35,7 +35,7 @@ async def start_cmd(client, message):
 
 @Client.on_message(filters.command("help") & filters.private)
 async def help_cmd(client, message):
-    tb = await message.reply("𝘏𝘢𝘷𝘪𝘯𝘨 𝘛𝘳𝘰𝘶𝘣𝘭𝘦? You're facing any problem while using a the bot or its commands contact admin 𝘣𝘦𝘭𝘰𝘸", reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("sᴜᴘᴘᴏʀᴛ ᴄʜᴀᴛ", url="https://t.me/EternalsHelplineBot")]]))
+    tb = await message.reply("<b>𝘏𝘢𝘷𝘪𝘯𝘨 𝘛𝘳𝘰𝘶𝘣𝘭𝘦? You're facing any problem while using a the bot or its commands contact admin 𝘣𝘦𝘭𝘰𝘸</b>", reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("sᴜᴘᴘᴏʀᴛ ᴄʜᴀᴛ", url="https://t.me/EternalsHelplineBot")]]))
     await asyncio.sleep(120)
     await tb.delete()
     try:
